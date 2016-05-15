5b7b5a1489cf68f44bf1b56d95dba73449f510c0
Eric Harper <complex451@gmail.com>
2016-05-15 10:24:38 -0600

    Username is hardcoded.  Will try and record match. Korean players only?

--- core.py
+++ core.py
@@ -1,2 +1,20 @@
+#!/user/bin/env python
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+# Load op.gg profile and click record button
+driver = webdriver.Firefox()
+driver.get('http://www.op.gg/summoner/userName=3%EB%85%84%EB%A7%8C%EC%9D%98%ED%9C%B4%EC%8B%9D')
 
+spec_button = driver.find_element_by_id('SpectateButton')
+spec_button.click()
+try:
+    record_button = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="ExtraView"]/div/div/div[1]/div/div'))
+    )
+    record_button.click()
+    print("Clicked record!")
+finally:
+    driver.quit()
